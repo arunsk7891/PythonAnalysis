a36d0e5dd16cdda5d99119df9a183587f98ce856
Arun <arunsk1610@gmail.com>
2024-07-08 17:11:03 +0530

    Update module1.py
    Importing Libraries and Datasets
    The libraries used are : 
    
    Pandas : For importing the dataset.
    Scikit-learn : For importing the model, accuracy module, and TfidfVectorizer.
    Warning : To ignore all the warnings
    Matplotlib : To plot the visualization. Also used Wordcloud for that.
    Seaborn : For data visualization.

--- module1.py
+++ module1.py
@@ -1,2 +1,11 @@
 print("Hello World")
 print("Test")
+
+import warnings 
+warnings.filterwarnings('ignore') 
+import pandas as pd 
+import re 
+import seaborn as sns 
+from sklearn.feature_extraction.text import TfidfVectorizer 
+import matplotlib.pyplot as plt 
+from wordcloud import WordCloud
